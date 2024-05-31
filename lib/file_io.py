845f7819d1ebab61812783b8beb5be73fa52127b
Alistairs <alistairs.gatheru@student.moringaschool.com>
2024-05-31 09:13:53 +0300

    manipulating files

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,18 @@
 def write_file(file_name, file_content):
-    pass
+    #Add the .txt extension
+    #Open the file in write mode and write the content
+    with open(f'{file_name}.txt', 'w') as file:
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    #Add the .txt extension
+    #Open the file in append mode and append the content
+    with open(f'{file_name}.txt', 'a') as file:
+        file.write(append_content)
+    
 
 def read_file(file_name):
-    pass
+    #Add the .txt extension
+    #Open file in read mode and return the content
+    with open(f'{file_name}.txt', 'r') as file:
+        return file.read()
